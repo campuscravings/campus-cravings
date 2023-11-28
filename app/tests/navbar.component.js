f3729c67485c5568dc31e63c374a14b80e6165bf
qckapiu <145004285+qckapiu@users.noreply.github.com>
2023-11-27 19:39:56 -1000

    updated testcafe for navbar

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -42,6 +42,16 @@ class NavBar {
     await testController.click('#navbar-sign-out');
   }
 
+  async goToProfilePage(testController) {
+    const visible = await Selector('#basic-navbar-nav').visible;
+    if (!visible) {
+      await testController.click('button.navbar-toggler');
+    }
+    await testController.expect(Selector('#navbar-current-user').exists).ok();
+    await testController.click('#navbar-current-user');
+    await testController.click('#profile-nav');
+  }
+
   /** Pull down login menu, go to sign up page. */
   async gotoSignUpPage(testController) {
     await this.ensureLogout(testController);
@@ -51,14 +61,6 @@ class NavBar {
     }
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-up');
-  }
-
-  async gotoUserProfilePage(testController) {
-    await testController.click('#user-profile-nav');
-  }
-
-  async gotoProfilePage(testController) {
-    await testController.click('#profile-nav');
   }
 
   async gotoUserHomePage(testController) {
