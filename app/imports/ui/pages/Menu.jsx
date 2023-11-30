2000f4d9628421e9c503c9f87f3caea85c6b7ea5
SafeAirplane <travisqu@hawaii.edu>
2023-11-30 10:51:01 -1000

    Created Delete function for Menu Items.

--- app/imports/ui/pages/Menu.jsx
+++ app/imports/ui/pages/Menu.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Card, Col, Row, Image, Container } from 'react-bootstrap';
+import { Card, Col, Row, Image, Container, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useParams } from 'react-router-dom';
+import { Roles } from 'meteor/alanning:roles';
+import { Trash } from 'react-bootstrap-icons';
 import { MenuItems } from '../../api/menuItems/MenuItems';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const Menu = () => {
+  const user = Meteor.user();
   const { vendorName } = useParams();
   const { menuItems, isLoading } = useTracker(() => {
     const subscription = Meteor.subscribe(MenuItems.publicationName);
@@ -18,6 +21,10 @@ const Menu = () => {
     return { menuItems: items, isLoading: !subscription.ready() };
   }, [vendorName]);
 
+  const removeItem = (itemId) => {
+    MenuItems.collection.remove(itemId);
+  };
+
   if (isLoading) {
     return <LoadingSpinner />;
   }
@@ -26,6 +33,69 @@ const Menu = () => {
     return <div>No menu items available for {vendorName}.</div>;
   }
 
+  if (Roles.userIsInRole(user, ['vendor', 'admin'])) {
+    return (
+      <Container id="menu-page" className="py-3">
+        <h1 className="text-center">{vendorName} Menu</h1>
+        <Row xs={1} md={2} lg={3} className="g-4 my-1">
+          {menuItems
+            .filter(item => item.available).map(item => (
+              <Col key={item._id}>
+                <Card>
+                  <Card.Body>
+                    <Container className="text-center">
+                      {item.vegan && (
+                        <Image
+                          src="/images/vegan_ICON.png"
+                          id="vegan-icon"
+                          style={{ position: 'absolute', top: '5px', right: '5px', maxWidth: '25px' }}
+                        />
+                      )}
+
+                      <Image
+                        id="menu-item-image"
+                        src={item.image}
+                        style={{ maxWidth: '300px', display: 'inline-block', border: '5px solid #ccc', position: 'relative' }}
+                        className="img-fluid"
+                      />
+                    </Container>
+                    <Row className="my-2">
+                      <Card.Title id="menu-item-name"><strong>{item.name}</strong></Card.Title>
+                    </Row>
+                    <div className="border p-3 my-1">
+                      <Card.Text id="menu-item-description">{item.description}</Card.Text>
+                    </div>
+                    <Row className="text-center my-1">
+                      <Col>
+                        <span id="menu-item-cost" className="badge bg-success rounded-pill">
+                          ${item.cost}
+                        </span>
+                      </Col>
+                      <Col>
+                        <span id="menu-item-calories" className="badge bg-secondary rounded-pill">
+                          Calories: {item.calories}
+                        </span>
+                      </Col>
+                    </Row>
+                    <Row className="text-center">
+                      <Col>
+                        <Button
+                          variant="danger"
+                          onClick={() => removeItem(item._id)}
+                          className="mb-2"
+                        >
+                          <Trash />
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    );
+  }
   return (
     <Container id="menu-page" className="py-3">
       <h1 className="text-center">{vendorName} Menu</h1>
@@ -76,6 +146,7 @@ const Menu = () => {
       </Row>
     </Container>
   );
+
 };
 
 export default Menu;
