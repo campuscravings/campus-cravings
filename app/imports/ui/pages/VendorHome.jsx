07e43dd159f5c3c5af50ac9e3e0268142633877d
qckapiu <145004285+qckapiu@users.noreply.github.com>
2023-12-06 06:53:48 -1000

    fixed single column issue

--- app/imports/ui/pages/VendorHome.jsx
+++ app/imports/ui/pages/VendorHome.jsx
@@ -88,9 +88,7 @@ const VendorHome = () => {
         src="../../images/paradise-palms.jpeg"
       />
       <Row xs={4} md={6} lg={8} className="colorBlockGreen">
-        <Col>
-          {menuItems.map((menuItem, index) => (<Col className="py-3" key={index}><MenuItem menuItem={menuItem} /></Col>))}
-        </Col>
+        {menuItems.map((menuItem, index) => (<Col className="py-3" key={index}><MenuItem menuItem={menuItem} /></Col>))}
       </Row>
     </Container>
   ) : <LoadingSpinner />);
