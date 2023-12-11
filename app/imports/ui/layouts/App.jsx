4cf9fc8af573baa5b486d56d9efe5269864b0b04
Kyle Bueche <38002610+kylebueche@users.noreply.github.com>
2023-12-10 19:41:20 -1000

    Removed stuff remnants

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -45,8 +45,6 @@ const App = () => {
           <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
           <Route path="/vendorhome" element={<VendorProtectedRoute ready={ready}><VendorHome /></VendorProtectedRoute>} />
           <Route path="/list" element={<ListVendors />} />
-          <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
-          <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
           <Route path="/admin" element={<AdminProtectedRoute ready={ready}><ListVendorsAdmin /></AdminProtectedRoute>} />
           <Route path="/adminhome" element={<AdminProtectedRoute ready={ready}><AdminHome /></AdminProtectedRoute>} />
           <Route path="/addvendor" element={<AdminProtectedRoute ready={ready}><AddVendor /></AdminProtectedRoute>} />
